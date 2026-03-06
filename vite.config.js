d11b2186fafd416ad2ee32d0deb70fc10b8e28fa
Jarg <jarg1976@gmail.com>
2026-03-06 16:52:53 +0000

    G4

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,25 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+
       manifest: {
         name: "Marvel Mask AR",
         short_name: "Mask Lab",
         description: "AR Superhero Mask Camera",
         theme_color: "#000000",
         background_color: "#000000",
+        start_url: "/Mask-Marval-App/",
+        scope: "/Mask-Marval-App/",
+        display: "standalone",
+
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/Mask-Marval-App/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/Mask-Marval-App/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
